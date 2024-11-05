5c0365fe32110aa3387f0c3f20a3139153f109b7
techwithtwin <realwebtechguru@gmail.com>
2024-11-05 14:54:09 +0300

    adding color mode toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,25 @@
 import AddMovieDrawer from "@/components/add-movie-drawer";
 import MovieCard from "@/components/movie-card";
+import ToggleColorMode from "@/components/toggle-color-mode";
 import { marginX } from "@/utils/constants";
 import { data } from "@/utils/constants/data";
-import { Flex, Heading, SimpleGrid, Stack } from "@chakra-ui/react";
+import { Flex, Heading, HStack, SimpleGrid, Stack } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Stack mx={marginX} my="3rem" gap="2rem">
-      <Flex gap={2} align="center">
-        <Heading>Movies Listing Total {data.length} </Heading>
-        <AddMovieDrawer />
-      </Flex>
+      <HStack
+        align="center"
+        justify="space-between"
+        flexDir={{ base: "column", sm: "row" }}
+      >
+        <Flex gap={2} align="center">
+          <Heading>Movies Listing Total {data.length} </Heading>
+          <AddMovieDrawer />
+        </Flex>
+        <ToggleColorMode />
+      </HStack>
+
       <SimpleGrid columns={{ base: 1, sm: 2, md: 4, lg: 6 }} gap={8}>
         {data.map((d) => (
           <MovieCard
